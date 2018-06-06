242a39ec5685c64f649ff9da5a534bb333aaec3b
StellarL <13688427004@163.com>
2018-06-06 08:29:09 +0800

    修改by_dy (#1)

--- app/src/main/java/com/example/packageapp/num1Activity.java
+++ app/src/main/java/com/example/packageapp/num1Activity.java
@@ -1,13 +1,35 @@
 package com.example.packageapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class num1Activity extends AppCompatActivity {
+
+    private Button btnLogin,btnRegist;
+    private EditText userName,psd;
+    private CheckBox rmpsd,autolgn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_num1);
+
+        btnLogin=findViewById(R.id.login);
+        btnRegist=findViewById(R.id.regist);
+        userName=findViewById(R.id.userName);
+        psd=findViewById(R.id.password);
+
+        btnRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                startActivityForResult(intent,1);
+            }
+        });
     }
 }
