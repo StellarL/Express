193eb6121631776b9fc5ab93e93d19fa75decd6d
Danicady <504830697@qq.com>
2018-06-11 18:00:35 +0800

    num2修改

--- app/src/main/java/com/example/packageapp/num2Activity.java
+++ app/src/main/java/com/example/packageapp/num2Activity.java
@@ -1,16 +1,18 @@
 package com.example.packageapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class num2Activity extends AppCompatActivity {
 
+    private int TAKE_PHOTO_WITH_DATA=1;
     private Button btnSubmit;
     private EditText name,userId;
     private ImageView cardUp,cardBack;
@@ -35,5 +37,28 @@ public class num2Activity extends AppCompatActivity {
             }
         });
 
+        cardUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,TAKE_PHOTO_WITH_DATA);
+            }
+        });
+        cardBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        final Bitmap photo = data.getParcelableExtra("data");
+        //不做处理直接粘贴
+        cardUp.setImageBitmap(photo);
+    }
+
 }
